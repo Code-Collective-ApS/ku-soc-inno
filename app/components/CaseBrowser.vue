4668d45dbf389609f95abd066e1156b6652870e8
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2025-11-20 16:18:48 +0100

    feat: add empty state to case browser

--- app/components/CaseBrowser.vue
+++ app/components/CaseBrowser.vue
@@ -19,21 +19,61 @@
               variant="link"
               trailing-icon="mdi-chevron-right"
               :to="`/cases/${c.id}`"
+              class="cursor-pointer"
               >Open</UButton
             >
           </div>
         </UPageCard>
+      </div>
+      <div v-if="cases.length === 0">
+        <UEmpty
+          icon="i-lucide-file"
+          title="Der er ikke oprettet nogle cases endnu."
+          description="Det ser ud til at der ikke eksisterer nogle cases i databasen. Du er velkommen til at oprette den første case."
+          :actions="
+            [
+              {
+                icon: 'i-lucide-plus',
+                label: 'Opret den første case',
+                color: 'success' as const,
+                class: 'cursor-pointer',
+                to: '/cases/new',
+              },
+              {
+                icon: 'i-lucide-refresh-cw',
+                label: 'Opdatér',
+                color: 'neutral',
+                variant: 'subtle' as const,
+                class: 'cursor-pointer',
+                onClick: async () => {
+                  await refreshCases();
+                  toast.add({
+                    title: 'Cases blev opdateret',
+                  });
+                },
+              },
+            ] satisfies ButtonProps[]
+          "
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { ButtonProps } from "#ui/types";
+
 defineProps({
   cases: {
     type: Array as PropType<CaseSerialized[]>,
     required: false,
     default: () => [],
   },
+  refreshCases: {
+    type: Function as PropType<() => Promise<void>>,
+    required: true,
+  },
 });
+
+const toast = useToast();
 </script>
