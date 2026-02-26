788e3a0f03648b08043c199b55ea1d5260db9266
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2026-02-26 17:10:52 +0100

    fix: remove declare global from type file handled by nuxt

--- shared/types/resource.d.ts
+++ shared/types/resource.d.ts
@@ -13,15 +13,13 @@ import type {
   SolutionAttachmentResponse as _SolutionAttachmentResponse,
 } from "~~/server/utils/resources/solution";
 
-declare global {
-  type CaseResponse = _CaseResponse;
-  type CaseSerialized = _CaseSerialized;
-  type SolutionResponse = _SolutionResponse;
-  type SolutionSerialized = _SolutionSerialized;
-  type SolutionPdfSerialized = _SolutionPdfSerialized;
-  type SolutionAttachmentSerialized = _SolutionAttachmentSerialized;
-  type SolutionIllustrationSerialized = _SolutionIllustrationSerialized;
-  type SolutionPdfResponse = _SolutionPdfResponse;
-  type SolutionAttachmentResponse = _SolutionAttachmentResponse;
-  type SolutionIllustrationResponse = _SolutionIllustrationResponse;
-}
+type CaseResponse = _CaseResponse;
+type CaseSerialized = _CaseSerialized;
+type SolutionResponse = _SolutionResponse;
+type SolutionSerialized = _SolutionSerialized;
+type SolutionPdfSerialized = _SolutionPdfSerialized;
+type SolutionAttachmentSerialized = _SolutionAttachmentSerialized;
+type SolutionIllustrationSerialized = _SolutionIllustrationSerialized;
+type SolutionPdfResponse = _SolutionPdfResponse;
+type SolutionAttachmentResponse = _SolutionAttachmentResponse;
+type SolutionIllustrationResponse = _SolutionIllustrationResponse;
